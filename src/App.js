a7853873717174dd8ba2e6a1d586387a2548a763
Morteza <morteza.abolfathi73@gmail.com>
2022-11-05 17:35:42 +0330

    update App.js add route planed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./style/app.css";
 import Layout from "./components/layout/layOut";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpComing from "./components/upComing/UpComing";
+import Planed from "./components/planed/Planed";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route path="/" element = {<Layout/>}/>
           <Route path="/upComing" element={<Layout> <UpComing/></Layout>}/>
+          <Route path="/planed" element={<Layout> <Planed/></Layout>}/>
       </Routes>
     </BrowserRouter>
     </div>
