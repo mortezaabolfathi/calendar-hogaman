7f3165ecc7ec05895a792b888fb95fe4f780dafd
Morteza <morteza.abolfathi73@gmail.com>
2022-11-05 20:37:32 +0330

    update App.js add route DesignWeekly

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Layout from "./components/layout/layOut";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpComing from "./components/upComing/UpComing";
 import Planed from "./components/planed/Planed";
+import LayoutWeekly from "./components/layout/LayoutWeekly"
+import DesignWeekly from "./components/designWeekly/DesignWeekly"
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
           <Route path="/" element = {<Layout/>}/>
           <Route path="/upComing" element={<Layout> <UpComing/></Layout>}/>
           <Route path="/planed" element={<Layout> <Planed/></Layout>}/>
+          <Route path="/LayOutWeekly" element={<LayoutWeekly> <DesignWeekly/></LayoutWeekly>}/>
       </Routes>
     </BrowserRouter>
     </div>
