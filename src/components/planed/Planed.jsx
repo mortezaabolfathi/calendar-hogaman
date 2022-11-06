28f41751238ef248e3f8637434da99055525e961
Morteza <morteza.abolfathi73@gmail.com>
2022-11-06 23:36:55 +0330

    update Planed.jsx and planed.css add item calendar and design

--- src/components/planed/Planed.jsx
+++ src/components/planed/Planed.jsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./../../style/planed.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faLocationDot, faClock} from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment/moment";
+import { getTime } from "../../reduxApp/features/calendar/calendar";
 
 const Planed = () => {
   const [showPlanedItem, setShowPlanedItem] = useState(false);
 
+  const dispatch = useDispatch();
+  const {clock} = useSelector(state=>state.calendar)
+  
+  dispatch(getTime(moment().format('LT')));
+
   useEffect(() => {
     setTimeout(() => {
       setShowPlanedItem(true);
-    }, 500);
+    }, 700);
   }, []);
-
+  
   return (
-    // <div className={showPlanedItem ? "planed-box-addItem" : "planed-box"}>
     <div className="planed-box-addItem">
-      <div className="upComing">
-        <div className="_upComing_box_designWeekly">
+      <div className="planed-box">
+        <div className="_planed_box_designWeekly">
           <Link to="/LayOutWeekly">
             <p>Design weekly</p>
-            <p>Location</p>
+            < div className="_planed_Location">
+              <p><FontAwesomeIcon icon={faLocationDot} /> Location</p>
+              <p><FontAwesomeIcon icon={faClock} /> {clock}</p>
+            </div>
           </Link>
         </div>
         {showPlanedItem ? (
           <div className="_planed_box_daily">
-            <p>Daily</p>
-            <p>Moscow</p>
+              <p>Daily</p>
+              <div className="_planed_Location_box_daily">
+              <p><FontAwesomeIcon icon={faLocationDot} /> Location</p>
+              <p><FontAwesomeIcon icon={faClock} /> {clock}</p>
+            </div>
           </div>
         ) : (
           ""
@@ -37,8 +52,12 @@ const Planed = () => {
               : "_upComing_box_designTasks"
           }
         >
-          <p>Design tasks details</p>
-          <p>Moscow</p>
+              <p>Design tasks Details</p>
+              <div className="_planed_Location_box_daily">
+              <p><FontAwesomeIcon icon={faLocationDot} /> Location</p>
+              <p><FontAwesomeIcon icon={faClock} /> {clock}</p>
+            </div>
+          
         </div>
       </div>
       <div className="_planed-nav">
