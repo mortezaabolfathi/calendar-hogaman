86cf14644154afb8aa3083e35c166c9e5b15d2f8
Morteza <morteza.abolfathi73@gmail.com>
2022-11-06 23:40:08 +0330

    update UpComing.jsx and upComing.css add calendar item and design page

--- src/components/upComing/UpComing.jsx
+++ src/components/upComing/UpComing.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./../../style/upComing.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faLocationDot, faClock} from "@fortawesome/free-solid-svg-icons";
+import moment from "moment/moment";
+import { useDispatch, useSelector } from "react-redux";
+import { getTime } from "../../reduxApp/features/calendar/calendar";
+
+
 const UpComing = () => {
+
+  const dispatch=useDispatch()
+  const {clock}=useSelector(state=> state.calendar)
+
+  dispatch(getTime(moment().format('LT')))
 
   return (
     <div className="box-setting">
@@ -11,11 +23,17 @@ const UpComing = () => {
         </Link>
         <div className="_upComing_box_designWeekly">
           <p>Design weekly</p>
-          <p>Location</p>
+          <div className="_upcoming_Location">
+           <p><FontAwesomeIcon icon={faLocationDot} /> Location</p>
+           <p><FontAwesomeIcon icon={faClock} /> {clock}</p>
+          </div>
         </div>
         <div className="_upComing_box_designTasks">
           <p>Design tasks details</p>
-          <p>Moscow</p>
+          <div className="_upcoming_Location">
+          <p><FontAwesomeIcon icon={faLocationDot} /> Moscow</p>
+          <p><FontAwesomeIcon icon={faClock} /> {clock}</p>
+          </div>
         </div>
       </div>
     </div>
